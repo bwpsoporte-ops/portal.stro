738f9627f6a95062cb787433b29eb2639ba383b1
bwpsoporte-ops <bwpsoporte@gmail.com>
2026-08-05 03:22:31 -0500

    Remove outputFileTracingIncludes from config

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,6 @@ const nextConfig: NextConfig = {
   },
 
   serverExternalPackages: ["pdfkit"],
-
-  outputFileTracingIncludes: {
-    "/*": ["./node_modules/pdfkit/js/data/**/*"],
-  },
 };
 
 export default nextConfig;
