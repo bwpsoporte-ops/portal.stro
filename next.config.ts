c60ac9f555248683ca8542f1e567ccabf14378c0
bwpsoporte-ops <bwpsoporte@gmail.com>
2026-08-05 03:19:26 -0500

    Add pdfkit to serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+
+  serverExternalPackages: ["pdfkit"],
+
+  outputFileTracingIncludes: {
+    "/*": ["./node_modules/pdfkit/js/data/**/*"],
+  },
 };
 
 export default nextConfig;
